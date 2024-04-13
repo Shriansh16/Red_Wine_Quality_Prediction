77ebb6f0ce17f18445c1334439f3bd27c2c5bfc8
Shriansh16 <shrianshsingh20@gmail.com>
2024-04-13 10:28:19 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages,setup
+from typing import List
+k='-e .'
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+        if k in requirements:
+            requirements.remove(k)
+
+setup(
+    name='Red_Wine',
+    version='0.0.1',
+    author='Shriansh',
+    author_email='shrianshsingh20@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
